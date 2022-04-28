6292847950bb5032893812d46e07bedefdfdf09f
growbook91 <70497021+growbook91@users.noreply.github.com>
2022-04-28 18:28:20 +0900

    firebase setting

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+
+  
   @override
   Widget build(BuildContext context) {
+
+    FirebaseFirestore.instance
+    .collection('Announcement')
+    .get()
+    .then((QuerySnapshot querySnapshot) {
+        querySnapshot.docs.forEach((doc) {
+            print(doc["text"]);
+        });
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text('HomeView'),
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // configureApp(); //remove # in url
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     GetMaterialApp(
       title: "Application",
