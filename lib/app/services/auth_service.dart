96462e957e8ae236f77c3c8f464651771c90bce8
growbook91 <70497021+growbook91@users.noreply.github.com>
2022-05-04 13:42:15 +0900

    google sign in complete

--- lib/app/services/auth_service.dart
+++ lib/app/services/auth_service.dart
@@ -26,4 +26,33 @@ class AuthService extends GetxService {
       print(error);
     }
   }
+
+  //구글 로그인을 통해 로그인을 하는 코드이다.
+  Future<void> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  await auth.value.signInWithCredential(credential);
+  print(auth.value.currentUser);
+}
+
+//구글 로그인에서 로그아웃 할때 설정해주었던 모든 것들을 다시 초기화 시켜주는 작업도 함께 해준다.
+  void googleSignOut() async {
+    // await auth.;
+    await _googleSignIn.signOut();
+    await auth.value.signOut();
+    print("before sign out: " + auth.value.currentUser.toString());
+    print("User Sign Out");
+    print("After sign out: " + auth.value.currentUser.toString());
+  }
 }
