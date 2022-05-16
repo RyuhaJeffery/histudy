b795e51788c454cd7c3ec8630671613d00892f90
Yechan Jung <asdxcv1998@gmail.com>
2022-05-16 18:07:11 +0900

    auth

--- lib/app/middleware/auth_middleware.dart
+++ lib/app/middleware/auth_middleware.dart
@@ -1,8 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../services/auth_service.dart';
 import '../routes/app_pages.dart';
+
+FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+Future<DocumentSnapshot> user = _firebaseFirestore
+    .collection("Profile")
+    .doc(AuthService.to.auth.value.currentUser!.uid).get();
 
 class EnsureAuthMiddleware extends GetMiddleware {
   @override
@@ -20,6 +27,16 @@ class EnsureAuthMiddleware extends GetMiddleware {
 }
 
 class EnsureNotAuthedMiddleware extends GetMiddleware {
+  @override
+  Future<GetNavConfig?> redirectDelegate(GetNavConfig route) async {
+    if (AuthService.to.auth.value.currentUser != null) {
+      return GetNavConfig.fromRoute(Routes.HOME);
+    }
+    return await super.redirectDelegate(route);
+  }
+}
+
+class EnsureSignUpMiddleware extends GetMiddleware {
   @override
   Future<GetNavConfig?> redirectDelegate(GetNavConfig route) async {
     if (AuthService.to.auth.value.currentUser != null) {
