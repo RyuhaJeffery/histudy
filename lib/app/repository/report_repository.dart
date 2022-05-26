66d002f514fcccd9540af2aca88d927b35c96f24
RyuhaJeffery <yws2121@naver.com>
2022-05-26 20:41:51 +0900

    tmp

--- lib/app/repository/report_repository.dart
+++ lib/app/repository/report_repository.dart
@@ -4,7 +4,8 @@ import 'package:get/get.dart';
 import 'package:histudy/app/models/report_model.dart';
 
 class ReportRepository {
-  static final reportCollection = FirebaseFirestore.instance.collection('Report');
+  static final reportCollection =
+      FirebaseFirestore.instance.collection('Report');
   static final groupCollection = FirebaseFirestore.instance.collection('Group');
 
   // static Future<List<ReportModel>> getReportList(String group) async {
@@ -17,35 +18,45 @@ class ReportRepository {
   //   }).toList();
   // }
 
-  static Stream<QuerySnapshot> getReportList(String group){
+  static Stream<QuerySnapshot> getReportList(String group) {
     return groupCollection.doc(group).collection('reports').snapshots();
   }
 
-  static uploadReport(String author, String code, DateTime codeDatetime, DateTime dateTime, String duration, String group, String image, List<String> participants, String studyStartTime, String text, String title) {
+  static uploadReport(
+      String author,
+      String code,
+      DateTime codeDatetime,
+      DateTime dateTime,
+      String duration,
+      String group,
+      String image,
+      List<String> participants,
+      String studyStartTime,
+      String text,
+      String title) {
     groupCollection
         .doc(group)
         .collection("reports")
         .doc(dateTime.toString())
         .set({
-      'author' : author,
-      'code' : code,
-      'codeDatetime' : codeDatetime,
-      'dateTime' : dateTime,
-      'duration' : int.parse(duration),
-      'group' : group,
-      'image' : "",
-      'participants' : participants,
-      'studyStartTime' : studyStartTime,
-      'text' : text,
-      'title' : title,
-      'sem' : "1",
-      'year' : "2022",
+      'author': author,
+      'code': code,
+      'codeDatetime': codeDatetime,
+      'dateTime': dateTime,
+      'duration': int.parse(duration),
+      'group': group,
+      'image': "",
+      'participants': participants,
+      'studyStartTime': studyStartTime,
+      'text': text,
+      'title': title,
+      'sem': "1",
+      'year': "2022",
     });
 
-    groupCollection
-      .doc(group).update({
-        'time' : FieldValue.increment(int.parse(duration)),
-        'count' : FieldValue.increment(1)
+    groupCollection.doc(group).update({
+      'time': FieldValue.increment(int.parse(duration)),
+      'meeting': FieldValue.increment(1)
     });
   }
 }
