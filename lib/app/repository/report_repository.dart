1d4fa0e96c14195c3172421abd2e33eea5bec677
RyuhaJeffery <yws2121@naver.com>
2022-09-25 23:55:12 +0900

    [ryu] study info update

--- lib/app/repository/report_repository.dart
+++ lib/app/repository/report_repository.dart
@@ -59,5 +59,20 @@ class ReportRepository {
       'time': FieldValue.increment(int.parse(duration)),
       'meeting': FieldValue.increment(1)
     });
+
+    groupCollection.doc(group).update({
+      'time': FieldValue.increment(int.parse(duration)),
+      'meeting': FieldValue.increment(1)
+    });
+
+    for (int i = 0; i < participants.length; i++) {
+      FirebaseFirestore.instance
+          .collection("Profile")
+          .doc(participants[i])
+          .update({
+        "${semId}_time": FieldValue.increment(int.parse(duration)),
+        "${semId}_meeting": FieldValue.increment(1)
+      });
+    }
   }
 }
